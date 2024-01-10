f8847009442d02e5bdf1ffd8a3af2c77a3b2a7a8
zhaoyuxuan <929644473@qq.com>
2024-01-10 10:45:42 +0800

    add

---
+++ MikanCat/View/BangumiListView.swift
@@ -0,0 +1,18 @@
+//
+//  BangumiListView.swift
+//  MikanCat
+//
+//  Created by 赵宇轩 on 2024/1/9.
+//
+
+import SwiftUI
+
+struct BangumiListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BangumiListView()
+}
